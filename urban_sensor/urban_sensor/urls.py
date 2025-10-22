823687e61814ff479e41e28898e15c26b0efd53d
Amanda <amandasmc2105gmail.com>
2025-10-21 22:12:05 -0300

    CRUD Direccion(revisar y arreglar)

--- urban_sensor/urban_sensor/urls.py
+++ urban_sensor/urban_sensor/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for proyecto_web project.
+URL configuration for urban_sensor project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('direcciones/', include('direccion.urls')),
 ]
