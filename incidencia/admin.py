9ce11a3d391159585ad1d12753bf075ef1e7942d
Diumd01 <127271054+Diumd01@users.noreply.github.com>
2025-10-22 23:25:36 -0300

    Delete incidencia directory

--- incidencia/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Departamento, Cuadrilla, TipoIncidencia, Incidencia
-
-admin.site.register(Departamento)
-admin.site.register(Cuadrilla)
-admin.site.register(TipoIncidencia)
-admin.site.register(Incidencia)
--- incidencia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include 
-from . import views
-
-urlpatterns = [
-    path('', views.lista_incidencias, name='lista_incidencias'),
-    path('<int:id>/', views.detalle_incidencia, name='detalle_incidencia'),
-    path('admin/', admin.site.urls),
-    path('incidencias/', include('incidencia.urls')), 
-]
